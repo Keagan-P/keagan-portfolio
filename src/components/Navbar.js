ca9d3f7449394f3799ca95a7b22e7eff46a5c7b9
unknown <pottieskeagan@gmail.com>
2025-05-10 12:31:32 +0200

    final changes2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Navbar.css';
 import logo from '../pictures/KPlogo.png';
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar" id="home">
       <div className="navbar-logo">
         <img src={logo} alt="KP Logo" className="logo-image" />
       </div>
-      <ul className="navbar-links">
+
+      
+      <div className="menu-toggle" onClick={toggleMenu}>
+        &#9776;
+      </div>
+
+    
+      <ul className={`navbar-links ${menuOpen ? 'show' : ''}`}>
         <li><a href="#home">Home</a></li>
         <li><a href="#services">Services</a></li>
         <li><a href="#about">About me</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
         <li><a href="#contact">Contact me</a></li>
       </ul>
+
       <div className="navbar-button">
       </div>
     </nav>
@@ -22,3 +36,4 @@ function Navbar() {
 }
 
 export default Navbar;
+
